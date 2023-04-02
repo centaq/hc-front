5016918db57e7e5874e103c10f1fab209e4df4c8
Centaq <marcinboniakowski@gmail.com>
2023-04-02 17:41:50 +0200

    Correct time rounding

--- src/helpers/FormatterHelper.ts
+++ src/helpers/FormatterHelper.ts
@@ -6,13 +6,13 @@ export class FormatterHelper {
         if (text > 9*3600)
             return "> 9 h";
         if (text < 120)
-            return text.toFixed(0) + " s";
+            return Math.floor(text) + " s";
         
         if (text >= 3600)
-            h_part = (text / 3600).toFixed(0);
+            h_part = Math.floor(text / 3600).toString();
         if (text >= 60) 
-            m_part = ((text % 3600) / 60).toFixed(0);
-        s_part = (text % 60).toFixed(0);
+            m_part = Math.floor((text % 3600) / 60).toString();
+        s_part = Math.floor(text % 60).toString();
         return h_part.padStart(2, '0') + ":" + m_part.padStart(2, '0') + ":" + s_part.padStart(2, '0');
     }
 }
