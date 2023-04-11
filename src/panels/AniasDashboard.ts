9d03b0740f7901e2e429e6a655c0f8457791720d
Centaq <marcinboniakowski@gmail.com>
2023-04-11 22:46:51 +0200

    Correct sensor assignment for heater activity

--- src/panels/AniasDashboard.ts
+++ src/panels/AniasDashboard.ts
@@ -15,7 +15,7 @@ export class AniasDashboard extends PanelBase implements IMainPanel {
             new TemperatureSensor('Temperatura ciepłej wody', "1.10.62"),
             new ActionMultiDevice("Ogrzewanie", [
                 { title: "Stan kotła", sensor: "10.21.3", type: ActionMultiSensorType.Preview, unit: "", valueFormatter: FormatterHelper.heaterState},
-                { title: "Aktywność kotła", sensor: "1.10.60", sensor1: "10.21.4", type: ActionMultiSensorType.Preview, unit: "%", valueFormatter: FormatterHelper.decimalToPercentage },
+                { title: "Aktywność kotła", sensor: "avg.cd.10.21.4", type: ActionMultiSensorType.Preview, unit: "%", valueFormatter: FormatterHelper.decimalToPercentage },
                 { title: "CO", sensor: "1.10.60", sensor1: "1.10.61", type: ActionMultiSensorType.Preview, unit: "°C" },
                 { title: "CWU", sensor: "1.10.62", sensor1: "1.10.63", sensor2: "1.10.70", type: ActionMultiSensorType.Preview, unit: "°C" },
                 { title: "Powrót", sensor: "1.10.64", type: ActionMultiSensorType.Preview, unit: "°C" },
